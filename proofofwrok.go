b483e4266878a0b92df5d5d375e7bf81bdf2c177
yun <liyun@idongjia.cn>
2018-08-30 09:42:36 +0800

    update part_4

--- proofofwrok.go
+++ proofofwrok.go
@@ -31,7 +31,7 @@ func (pow *ProofOfWork) prepareData(nonce int) []byte {
 	data := bytes.Join(
 		[][]byte{
 			pow.block.PrevBlockHash,
-			pow.block.Data,
+			pow.block.HashTransactions(),
 			Int2Hex(pow.block.TimeStamp),
 			Int2Hex(int64(targetBits)),
 			Int2Hex(int64(nonce)),
@@ -44,10 +44,11 @@ func (pow *ProofOfWork) Run() (int, []byte) {
 	var hashInt big.Int
 	var hash [32]byte
 	nonce := 0
-	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+	fmt.Printf("Mining a new block")
 	for nonce < maxNonce {
 		data := pow.prepareData(nonce)
 		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
 		hashInt.SetBytes(hash[:])
 		if hashInt.Cmp(pow.target) == -1 {
 			break
